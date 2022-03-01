dda06af8b6efe2b24683be121649382fd5458716
joshua.lu <hmlu36@gmail.com>
2022-03-02 06:48:58 +0800

    'deploy'

--- docs/.vitepress/components/bill.vue
+++ docs/.vitepress/components/bill.vue
@@ -1,61 +1,85 @@
 <template>
-  <div>
-    <DoughnutChart :chartData="testData" />
-  </div>
+  <label><input type="radio" name="billType" value="Gas" v-model="billType" @click="getBillData" />瓦斯</label>
+  <label><input type="radio" name="billType" value="Water" v-model="billType" @click="getBillData"/>水費</label>
+  <label><input type="radio" name="billType" value="Electricity" v-model="billType" @click="getBillData"/>電費</label>
+  <br />
+  <vue-highcharts
+    type="chart"
+    :options="chartOptions"
+    :redrawOnUpdate="true"
+    :oneToOneUpdate="false"
+    :animateOnUpdate="true"
+  />
 </template>
 
 <script>
-import { onMounted } from 'vue';
-import axios from 'axios';
+import { onMounted, computed, ref } from "vue";
+import axios from "axios";
 
-import { DoughnutChart } from 'vue-chart-3';
-import { Chart, registerables } from 'chart.js';
-
-Chart.register(...registerables);
+import VueHighcharts from "vue3-highcharts";
 
 export default {
   data: () => ({
     records: [],
   }),
-  components: { DoughnutChart },
-  async setup() {
-    const billData = async (billType) => {
-      const url = `https://api.airtable.com/v0/${
-        import.meta.env.VITE_APP_ID_BILL
-      }/${typeof billType !== 'undefined' ? billType : 'Gas'}`;
-      console.log(url);
-      return await axios.get(url, {
-        headers: {
-          Authorization: 'Bearer ' + import.meta.env.VITE_APP_KEY,
-          'Content-Type': 'application/json',
-        },
-      });
+  components: {
+    VueHighcharts,
+  },
+  setup() {
+    const billData = ref([]);
+    const billType = ref("Gas");
+    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_APP_ID_BILL}/${
+      billType.value
+    }`;
+    const getBillData = async () => {
+      console.log(billType.value);
+      try {
+        const response = await axios.get(url, {
+          headers: {
+            Authorization: "Bearer " + import.meta.env.VITE_APP_KEY,
+            "Content-Type": "application/json",
+          },
+        });
+        billData.value = response.data.records.map((entry) => entry.fields);
+        console.log(JSON.stringify(billData.value.map((entry) => entry.年月)));
+      } catch (e) {
+        console.log(e);
+      }
     };
 
-    console.log(
-      JSON.stringify(
-        (await billData()).data.records.map((entry) => entry.fields)
-      )
-    );
+    onMounted(async () => {
+      getBillData();
+    });
 
-    const testData = {
-      labels: ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre'],
-      datasets: [
+    const seriesData = ref([25, 39, 30, 15]);
+    const categories = ref(["Jun", "Jul", "Aug", "Sept"]);
+
+    const chartOptions = computed(() => ({
+      chart: {
+        type: "line",
+      },
+      title: {
+        text: "年月",
+      },
+      xAxis: {
+        categories: billData.value
+          .map((entry) => entry.年月)
+          .sort((a, b) => a.localeCompare(b)),
+      },
+      yAxis: {
+        title: {
+          text: "費用",
+        },
+      },
+      series: [
         {
-          data: [30, 40, 60, 70, 5],
-          backgroundColor: [
-            '#77CEFF',
-            '#0079AF',
-            '#123E6B',
-            '#97B0C4',
-            '#A5C8ED',
-          ],
+          name: "帳單資訊",
+          data: billData.value.map((entry) => Number(entry.費用)),
         },
       ],
-    };
+    }));
 
-    return { testData , billData};
-    onMounted(() => {});
+    return { chartOptions, getBillData, billType };
   },
 };
 </script>
